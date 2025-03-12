78e82a0863a36548bdcf57f179ce7304f5a4ca68
rafidulonnoy <rafidulonnoy@gmail.com>
2025-03-12 21:16:21 +0600

    12/03/2025 some changes

---
+++ 3rdSemester/areaOfSquare.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n,square;
+  cout<<"Enter the size of the sides: ";
+  cin>>n;
+  square=n*n;
+  cout<<"So the area of the square is: "<<square<<endl;
+}
